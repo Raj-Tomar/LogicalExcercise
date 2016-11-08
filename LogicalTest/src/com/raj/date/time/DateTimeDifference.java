41ce4f6c25f36a4b11f8b9c76d284ebb7c13f7bf
Raj Tomar <rajtomar50@gmail.com>
2016-11-08 14:26:27 +0530

    DateTime difference from UTC Date
    Signed-off-by: Raj Tomar <rajtomar50@gmail.com>

--- LogicalTest/src/com/raj/date/time/DateTimeDifference.java
+++ LogicalTest/src/com/raj/date/time/DateTimeDifference.java
@@ -3,6 +3,7 @@ package com.raj.date.time;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -17,16 +18,16 @@ public class DateTimeDifference {
 
 		DateFormat pstFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		pstFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		String date3 = pstFormat.format(new Date());
-		
+
 		System.out.println(date3);
-		
+
 		//in milliseconds
 		long difference = date2.getTime() - date1.getTime();
 
 		System.out.println(difference + " Miliseconds ");
-		
+
 		long diffSeconds = difference / 1000 % 60;
 		long diffMinutes = difference / (60 * 1000) % 60;
 		long diffHours 	 = difference / (60 * 60 * 1000) % 24;
@@ -36,5 +37,26 @@ public class DateTimeDifference {
 		System.out.println(diffMinutes + " minutes, ");
 		System.out.println(diffHours + " hours, ");
 		System.out.println(diffDays + " days, ");
+
+		ZonedDateTime zd1 = ZonedDateTime.parse("2016-11-06T02:56:02.038" + "Z");
+		ZonedDateTime zd2 = ZonedDateTime.parse("2016-11-08T22:56:02.038" + "Z");
+		long millis = zd2.toInstant().toEpochMilli() - zd1.toInstant().toEpochMilli();
+		System.out.println(millis);
+		long noOfDay = millis / (24 * 60 * 60 * 1000);
+		System.out.println(noOfDay);
+		
+		String s1 = zd1.toLocalDate().toString() + " " + zd1.toLocalTime().toString();
+		String s2 = zd2.toLocalDate().toString() + " " + zd2.toLocalTime().toString();
+		System.out.println("date1 : "+s1 );
+		System.out.println("date2 : "+s2 );
+
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		df.setTimeZone(TimeZone.getTimeZone("GMT"));
+		Date dt1 = df.parse(s1);
+		Date dt2 = df.parse(s2);
+		long milieSeconds = dt2.getTime() - dt1.getTime();
+		System.out.println(milieSeconds);
+		long days = milieSeconds / (24 * 60 * 60 * 1000);
+		System.out.println(days);
 	}
 }
